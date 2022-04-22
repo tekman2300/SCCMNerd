2d45d9aed2c3d84b659f4c37365e1a5b03bca4d4
tekman2300 <51452269+tekman2300@users.noreply.github.com>
2022-04-22 09:46:46 -0500

    ADInfo

---
+++ ADInfo/CopyfilesAD.ps1
@@ -0,0 +1,2 @@
+# Copy all files to modules directory
+Copy-Item "ActiveDirectory" -Destination "C:\Program Files\WindowsPowerShell\Modules\" -Recurse
---
+++ ADInfo/GetUserInfo.ps1
@@ -0,0 +1,14 @@
+﻿Import-Module ActiveDirectory
+Add-Type -AssemblyName PresentationCore,PresentationFramework
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+Add-Type -AssemblyName Microsoft.VisualBasic
+$ButtonType = [System.Windows.Forms.MessageBoxButtons]::OK
+$MessageIcon = [System.Windows.Forms.MessageBoxIcon]::Information
+$MessageTitle = 'Retrieve UserName'
+$title = 'Retrieve UserName'
+$msg   = 'Enter email address (firstname.lastname@domain.com) Press Ctrl-C to exit:'
+$text = [Microsoft.VisualBasic.Interaction]::InputBox($msg, $title)
+$User = $text
+$MessageBody = Get-AdUser -Filter "mail -eq '$User'" | Select-Object -ExpandProperty SamAccountName
+$Result = [System.Windows.Forms.MessageBox]::Show("$Messagebody",$MessageTitle,$ButtonType,$MessageIcon)
